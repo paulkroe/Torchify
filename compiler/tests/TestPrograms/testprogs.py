e6d0c926822c43db0138832d8ceb3cbff0bfb41c
Paul Kroeger <paul.kroeger@columbia.edu>
2024-12-08 01:21:58 -0500

    removed parse trees from output

--- compiler/tests/TestPrograms/testprogs.py
+++ compiler/tests/TestPrograms/testprogs.py
@@ -1,7 +1,7 @@
 from LexicalPhase import Lexer
 from SyntacticPhase import ll1_parse, parse_tree_to_ast, print_tree
 from CodeGenerationPhase.codegenerator import generate_code
-for i in range(0, 17):
+for i in range(10, 17):
     with open(f'compiler/tests/TestPrograms/prog{i}.txt', 'r') as file:
         file_contents = file.read()
 
@@ -13,8 +13,8 @@ for i in range(0, 17):
     print(lexer.token_stream)
     valid, parse_tree = ll1_parse(lexer.token_stream)
     if valid:
-        print(f"==================================================== PARSE TREE {i} =================================================")
-        print_tree(parse_tree)
+        # print(f"==================================================== PARSE TREE {i} =================================================")
+        # print_tree(parse_tree)
         ast = parse_tree_to_ast(parse_tree)
         print(f"======================================================== AST {i} =====================================================")
         print_tree(ast)
